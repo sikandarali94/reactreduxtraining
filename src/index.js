dd5181c55b8a7237f8e1ee0bdc115f6cab89c15f
Sikandar Ali <sikandarali94@hotmail.com>
2019-02-27 09:30:02 +1100

    finished sec1 lec10 javascript module systems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+// To show something to the page using React, we should follow these steps:
+
+// 1. Import the React and ReactDOM libraries. We are using the bundler Webpack to import the libraries.
 import React from 'react';
+/* In NodeJS, typically we use require statement rather than the import statement like this:
+const React = require('react);
+The import statement uses ES2015 modules system to grab code from other packages while the require statement uses
+CommonJS modules system.
+ */
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 2. Create a React component.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// 3. Take the React component and render it on the page.
