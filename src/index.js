b65c1ea099a55602c599d9e699da1163de0c8783
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-20 10:38:39 +1000

    finished sec4 lec48 getting a users physical location

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* The first callback function of getCurrentPosition() is run when the browser successfully receives a response. The
+second callback function runs when the browser unsuccessfully receives a response.
+ */
+
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+    );
+
     return <div>Hi there!</div>
 };
 
