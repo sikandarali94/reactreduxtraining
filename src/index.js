bdd89dce1bb5345199509e1844b2a656fa9c542e
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-20 10:54:51 +1000

    finished sec4 lec50 handling async operations with functional components

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 /* The first callback function of getCurrentPosition() is run when the browser successfully receives a response. The
 second callback function runs when the browser unsuccessfully receives a response.
  */
-
+/* With functional components we do not really have any good way of handling async operations like using Geolocation
+API with the getCurrentPosition() method.
+ */
 const App = () => {
     window.navigator.geolocation.getCurrentPosition(
         position => console.log(position),
         err => console.log(err)
     );
 
-    return <div>Hi there!</div>
+    return <div>Latitude: </div>
 };
 
 ReactDOM.render(
