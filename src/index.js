f08ae40bef9051c80d07e3cf71c82c885f196fe2
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-21 08:14:19 +1000

    finished sec5 lec53 initializing state through constructors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ import ReactDOM from 'react-dom';
 5. State can ONLY be updated using the function 'setState' (VERY IMPORTANT).
  */
 class App extends React.Component {
+    /* We can initialize state using the constructor method.
+     */
+    constructor(props) {
+        /* super() is a reference to the parent's (React.Component) constructor method. We need React.Component's
+        constructor method to run to set up all the code needed to create a React component, because the constructor
+        method in the child class (App) overwrites the constructor method in the parent class. super() should be called
+        in the child class constructor method before any other code, as shown below.
+         */
+        super(props);
+
+        this.state = { lat: null }; // Initialize state
+    }
+
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
