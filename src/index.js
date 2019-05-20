eff0296cb65cac9838c150a3f578a5c3bf6e4758
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-21 07:46:33 +1000

    finished sec4 lec51 refactoring from functional to class components

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ second callback function runs when the browser unsuccessfully receives a respons
 /* With functional components we do not really have any good way of handling async operations like using Geolocation
 API with the getCurrentPosition() method.
  */
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
+/* Here are some rules of class components:
+1. Must be a JavaScript class.
+2. Must extend (subclass) React.Component, which allows us to get a ton of functionality defined in React.Component into
+our class.
+3. Must define a 'render' method that returns some amount of JSX.
+ */
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
 
-    return <div>Latitude: </div>
-};
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(
     <App />,
