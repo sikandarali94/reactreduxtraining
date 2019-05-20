b3193494abb5c4a4ae55f42f861c9b9bdd2e79d1
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-20 10:26:40 +1000

    finished sec4 lec47 scaffolding the app

--- src/index.js
+++ src/index.js
@@ -1,54 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 
-import CommonDetail from './CommonDetail';
-import ApprovalCard from './ApprovalCard';
-
-/* The general rule for deciding whether to use a Functional component or a Class component is:
-    Function components are good for simple content, while Class components are good for just about everything else.
- */
-/* Here are a few benefits to using Class components:
-    1. Easier code organisation.
-    2. Can use 'state' (another React system).
-    3. Easier to handle user input.
-    4. Understands lifecycle events.
-    5. Easier to do things when the app first starts.
- */
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <h4>Warning!</h4>
-                Are you sure you want to do this?
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommonDetail
-                    avatar={faker.image.avatar()}
-                    author="Sam"
-                    timeAgo="Today at 4:45PM"
-                    blogPost="Hello there people."
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommonDetail
-                    avatar={faker.image.avatar()}
-                    author="Alex"
-                    timeAgo="Today at 5:45PM"
-                    blogPost="Bye guys. Have fun."
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommonDetail
-                    avatar={faker.image.avatar()}
-                    author="Jane"
-                    timeAgo="Yesterday at 6:45PM"
-                    blogPost="Feeling sad, despite my profile pic."
-                />
-            </ApprovalCard>
-        </div>
-    );
+    return <div>Hi there!</div>
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
-
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
