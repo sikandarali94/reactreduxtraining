60997e4b95dc85292720abe132683c7ed6bac586
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-21 11:22:37 +1000

    finished sec6 lec59 why lifecycle methods?

--- src/index.js
+++ src/index.js
@@ -10,6 +10,20 @@ import ReactDOM from 'react-dom';
 4. componentDidUpdate (called when component updates itself) -> sit and wait until this component is no longer shown
 5. componentWillUnmount (called when component is no longer shown)
  */
+
+/* Here are some guidelines associated with using each Component Lifecycle method:
+1. constructor: good place to do one-time setup; recommended not to data-loading here even though we can
+2. render: avoid doing anything besides returning JSX
+3. componentDidMount: good place to do data-loading!
+4. componentDidUpdate: good place to do more data-loading when state/props change
+5. componentWillUnmount: good place to do cleanup (especially for non-React stuff)
+ */
+
+/* Here are other lifecycle methods that are rarely used:
+1. shouldComponentUpdate
+2. getDerivedStateFromProps
+3. getSnapshotBeforeUpdate
+ */
 class App extends React.Component {
     constructor(props) {
         super(props);
