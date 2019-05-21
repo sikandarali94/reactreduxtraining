1fe213cb6ac06e60e009cfd113af157b9d5a990d
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-21 10:57:52 +1000

    finished sec6 lec58 introducing lifecycle methods

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* Here are some rules of state in React:
-1. Only usable with class components (technically can be used with functional components using the 'hooks' system).
-2. 'State' is a JS object that contains data strictly relevant to a component.
-3. Updating 'state' on a component causes the component to (almost) instantly rerender.
-4. State must be initialized when a component is created.
-5. State can ONLY be updated using the function 'setState' (VERY IMPORTANT).
+/* The other way to initialize state is using the lifecycle methods.
+ */
+/* Here is a general overview of a Component Lifecycle in order of time:
+1. constructor()
+2. render() -> content is visible on screen
+3. componentDidMount (runs once when component is rendered on screen) -> sit and wait for updates...
+4. componentDidUpdate (called when component updates itself) -> sit and wait until this component is no longer shown
+5. componentWillUnmount (called when component is no longer shown)
  */
 class App extends React.Component {
-    /* We can initialize state using the constructor method.
-     */
     constructor(props) {
-        /* super() is a reference to the parent's (React.Component) constructor method. We need React.Component's
-        constructor method to run to set up all the code needed to create a React component, because the constructor
-        method in the child class (App) overwrites the constructor method in the parent class. super() should be called
-        in the child class constructor method before any other code, as shown below.
-         */
         super(props);
 
-        /* Initializing state is the only time we do direct assignment to this.state.
-         */
-        this.state = { lat: null, errorMessage: '' }; // Initialize state
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
@@ -33,9 +26,14 @@ class App extends React.Component {
         );
     }
 
-    /* The render() method is called many times so it is not a good place to initialize some work or some request like
-    we do with calling the getCurrentPosition() method.
-     */
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+    }
+
+    componentDidUpdate() {
+        console.log('My component has just updated - it rerendered!');
+    }
+
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
