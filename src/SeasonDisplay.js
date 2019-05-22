66cbc78e1f568477693379ccdda4aad2daff11f4
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-22 10:58:05 +1000

    finished sec6 lec65 showing icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,10 +10,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
 
     return (
         <div>
+            <i className={`${icon} icon`}/>
             <h1>{season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach'}</h1>
+            <i className={`${icon} icon`}/>
         </div>
     );
 };
