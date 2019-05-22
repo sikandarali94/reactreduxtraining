0e725e72fe653141654f870d465e0c12de0042e2
Sikandar Ali <sikandarali94@hotmail.com>
2019-05-22 11:36:27 +1000

    finished sec6 lec68 showing a loading spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 /* The other way to initialize state is using the lifecycle methods.
  */
@@ -51,7 +52,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>;
+        return <Spinner />;
     }
 }
 
