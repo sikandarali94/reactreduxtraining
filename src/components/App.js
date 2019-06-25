cb775cff241f673ac02234f6538838a8966aff6d
Sikandar Ali <sikandarali94@hotmail.com>
2019-06-25 10:39:54 +1000

    finished sec8 lec89 axios vs fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    /* It is not the job of React to make a HTTP request, rather we have a separate code/library that handles these
+    requests (fetch [built into modern browsers] OR axios [3rd party package]). We will use the Axios library because
+    it takes less code to implement it as compared to using Fetch, and it handles edge cases for us.
+     */
     onSearchSubmit(term) {
-        console.log(term);
+
     }
 
     render() {
