0ce8165a84288dbae2543ecaa1ac42e3fb971a55
Sikandar Ali <sikandarali94@hotmail.com>
2019-06-28 11:40:48 +1000

    finished sec8 lec91 handling requests with async await

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,20 @@ class App extends React.Component {
     requests (fetch [built into modern browsers] OR axios [3rd party package]). We will use the Axios library because
     it takes less code to implement it as compared to using Fetch, and it handles edge cases for us.
      */
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID cc780cc1bf95fdb1d4276c7922a565c8bd1ebdaf2ac4d738bec9ee11de126b12'
             }
         });
+        // We could use a .then() method to write code that executes after async call is successful. Alternatively we
+        // can use async/await which is a lot easier to write.
+        // .then(response => {
+        //     console.log(response.data.results);
+        // });
+
+        console.log(response.data.results);
     }
 
     render() {
