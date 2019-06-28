9bdbda29cf1435185a4e11477834b5693f04671a
Sikandar Ali <sikandarali94@hotmail.com>
2019-06-28 17:07:32 +1000

    finished sec9 lec98 the purpose of keys in lists

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -6,6 +6,12 @@ const ImageList = (props) => {
             return <img src={image.urls.regular} />
         });
      */
+    /* To allow React to render a list of items smartly by analysing the DOM and figuring out what DOM element in the
+    list is missing (or needs to add) as the list updates, we need to place a key prop on each of the DOM element in the
+    DOM list. To implement this key prop we usually have some data in the array list that can be used as the value for
+    the key props (e.g. unique id values in the array list). If we don't have key props in DOM elements of a DOM list,
+    React throws an error.
+     */
     const images = props.images.map((image) => {
         return <img src={image.urls.regular} />
     });
