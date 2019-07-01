3c18e924af28173b15039677a539390616b1c7aa
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-01 12:25:56 +1000

    finished sec10 lec104 accessing image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        /* This will log clientHeight as 0. This is because we are console logging this value out before the image had
+        a chance to load itself up.
+         */
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     /* React Refs give us access to a single DOM element. We create refs in the constructor assign them to instance
     variables, then pass to a particular JSX element as props.
      */
@@ -10,6 +23,7 @@ class ImageCard extends React.Component {
         return (
             <div>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
