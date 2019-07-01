816649fea9701a663496465162d8eef06eb6740d
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-01 12:30:44 +1000

    finished sec10 lec105 callbacks on image load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -11,8 +11,13 @@ class ImageCard extends React.Component {
         /* This will log clientHeight as 0. This is because we are console logging this value out before the image had
         a chance to load itself up.
          */
-        console.log(this.imageRef.current.clientHeight);
+        /* When the image has loaded the 'load' event fires on the img element that we have placed the ref on. */
+        this.imageRef.current.addEventListener('load', this.setSpans);
     }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    };
 
     /* React Refs give us access to a single DOM element. We create refs in the constructor assign them to instance
     variables, then pass to a particular JSX element as props.
