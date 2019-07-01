612580e42c396b535eb327bc1f668f8bc2a6365a
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-01 11:11:59 +1000

    finished sec10 lec103 accessing the dom with refs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    /* React Refs give us access to a single DOM element. We create refs in the constructor assign them to instance
+    variables, then pass to a particular JSX element as props.
+     */
     render() {
         const { description, urls } = this.props.image;
 
