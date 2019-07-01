12934bccee96192d1dfa0f8dee24572717cf40d3
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-01 10:46:25 +1000

    finished sec10 lec100 grid css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,3 +1,4 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
@@ -16,7 +17,7 @@ const ImageList = (props) => {
         return <img alt={description} key={id} src={urls.regular} />
     });
 
-    return <div>{ images }</div>;
+    return <div className="image-list">{ images }</div>;
 };
 
 export default ImageList;
