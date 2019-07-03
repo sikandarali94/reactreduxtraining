9f0d11341716569930140585892472b238bc02b8
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-03 11:03:42 +1000

    finished sec11 lec110 scaffolding the app

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID cc780cc1bf95fdb1d4276c7922a565c8bd1ebdaf2ac4d738bec9ee11de126b12'
-    }
-});
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,8 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: 'Hi there!' };
-
-    onFormSubmit = (event) => {
-        event.preventDefault();
-        this.props.whenUserSubmits(this.state.term);
-    };
-
     render() {
-        return (
-            <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                    <input
-                        type="text"
-                        value={this.state.term}
-                        onChange={e => this.setState({ term: e.target.value })}
-                    />
-                    </div>
-                </form>
-            </div>
-        );
+        return <div>SearchBar</div>
     }
 }
 
