1c2fbdafb387f439bdbcd7d57f034a18d53f412d
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-09 10:24:48 +1000

    finished sec11 lec119 rendering a list of videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
-import VideoList from './VIdeoList';
+import VideoList from './VideoList';
 
 class App extends React.Component {
     state = { videos: [] };
--- src/components/VIdeoList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const VideoList = props => {
-    return <div>{props.videos.length}</div>;
-};
-
-export default VideoList;
