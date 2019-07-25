c8c245ebce7f7813904efbccdd9ab1b466599c24
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-25 10:53:45 +1000

    finished sec13 lec142 how react-redux works

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,10 @@
+/* React-Redux provides two components/tags to interact with the store: Provider and Connect (communicates with the
+Provider via the Context system). Provider is instantiated at the highest level of our application (even above the App
+component). We wrap components with an instance of the Connect component/tag that want to reference state properties in
+the store. We tell the Connect components what pieces of the state in the store to expose as props to the components
+they are wrapped around and different Action Creators we want to have wired up as well.
+a
+ */
 import React from 'react';
 
 const App = () => {
