181345542581b635f03c5f1446d521d9c51de4d2
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-25 12:06:53 +1000

    finished sec13 lec148 configuring connect with mapstatetoprops

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -9,4 +9,13 @@ class SongList extends Component {
     }
 }
 
-export default connect()(SongList);
+/* By convention we call the function below mapStateToProps (although we can call it whatever we like). This tells
+connect() what piece/s of state we want to reference from the store. In the example below 'songs' will be a prop of
+SongList that references the 'state.songs' property in the store. We also get access to the dispatch method in the
+props of SongList.
+ */
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
