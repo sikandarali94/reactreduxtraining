8ada4e0d48ca4fdd9b2f33aad13a655f4be6847f
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-30 11:56:08 +1000

    finished sec15 lec167 rules of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,10 @@
+/* Having all of our reducers in a single file will not scale well in large applications. So we should separate our
+reducers in separate file, import them all into this index.js file, and then combine them using the combineReducers
+method, as shown below.
+ */
 import { combineReducers } from "redux";
+import postsReducer from './postsReducer';
 
-export default combineReducers({});
+export default combineReducers({
+    posts: postsReducer
+});
