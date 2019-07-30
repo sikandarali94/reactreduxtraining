bc6da08e27109d7901bd724c0ea368e6403386eb
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-30 10:29:52 +1000

    finished sec14 lec162 understanding async action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,12 @@ export const fetchPosts = async () => {
     We get the error:
     'Actions must be plain objects. Use custom middleware for async actions.'
     redux-thunk is middleware that we will use to solve the error above.*/
+    /* Here are the two reasons we get the error above:
+    1. Action creators must return plain JS objects with a type property - we are not. Because we are using async/await
+    syntax which causes fetchPosts to return "jsonPlaceholder.get('/posts')" rather than "{type: 'FETCH_POSTS', payload:
+    response }" when data has not been fetched from the API.
+    2. By the time our action gets to a reducer, we won't have fetched our data!
+     */
     const response = await jsonPlaceholder.get('/posts');
 
     // Bad approach.
