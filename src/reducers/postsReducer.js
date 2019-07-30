aa5c7ac177f5a4bc2a874d7a1f68b4910a887ae1
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-30 12:01:01 +1000

    finished sec15 lec168 return values from reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,6 @@
 export default () => {
+    /* A reducer can never ever return undefined during initialization. It must always return a value besides undefined,
+    as shown below.
+     */
     return 123;
 };
