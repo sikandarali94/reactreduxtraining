2b5e2abede2ef8ff4e2ec66e2e3c2601a08b08be
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-30 12:34:11 +1000

    finished sec15 lec170 pure reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -5,5 +5,11 @@ export default () => {
     /* We should always set an initial value of the arguments passed into a reducer. e.g.
     const selectedSongReducer = (selectedSong = null, action) => {};
      */
+    /* Here are some rules for reducers:
+    1. Must return any value besides 'undefined'.
+    2. Produces 'state', or data to be used inside of our app using only previous state and the action.
+    3. Must not reach 'out of itself' (e.g. api request) to decide what value to return (reducers are pure).
+    4. Must not mutate its input 'state' argument.
+     */
     return 123;
 };
