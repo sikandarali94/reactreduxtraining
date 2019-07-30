aa792acb0230465f42adbb0bab8e781ba188d7f5
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-30 10:15:13 +1000

    finished sec14 lec161 making a request from an action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+    /* It is a bad approach to set an await expression to a variable and then putting that variable directly into the
+    payload of an action.
+    We get the error:
+    'Actions must be plain objects. Use custom middleware for async actions.'
+    redux-thunk is middleware that we will use to solve the error above.*/
+    const response = await jsonPlaceholder.get('/posts');
+
+    // Bad approach.
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
 };
