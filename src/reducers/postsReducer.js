b549d09b41fd11156f147d5e3b9cff5c4c4f2e4f
Sikandar Ali <sikandarali94@hotmail.com>
2019-07-30 12:27:36 +1000

    finished sec15 lec169 argument values

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,5 +2,8 @@ export default () => {
     /* A reducer can never ever return undefined during initialization. It must always return a value besides undefined,
     as shown below.
      */
+    /* We should always set an initial value of the arguments passed into a reducer. e.g.
+    const selectedSongReducer = (selectedSong = null, action) => {};
+     */
     return 123;
 };
