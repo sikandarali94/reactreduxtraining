8e9a8c95ce0cf70a65435c95dfc9a02cb2df71d1
Sikandar Ali <sikandarali94@hotmail.com>
2019-08-29 14:04:41 +1000

    finished sec15 lec190 quick refactor with chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -57,8 +57,12 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     else.
      */
     await dispatch(fetchPosts());
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 };
 
 export const fetchPosts = () => async dispatch => {
