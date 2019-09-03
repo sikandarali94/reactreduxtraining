53469ca4fedddd7df9aa3fde5ae8418b46b32d9c
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-03 10:52:39 +1000

    finished sec16 lec196 introducing react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
+/* To install React Router from npm, we write this command: npm install --save react-router-dom. We must be careful
+* not to install react-router. */
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>PageOne</div>
+};
+
+const PageTwo = () => {
+    return (
+        <div>
+            PageTwo
+            <button>Click Me!</button>
+        </div>
+    );
+};
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    {/* We use the exact attribute to match the entire URL, rather than as part of the URL. */}
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
