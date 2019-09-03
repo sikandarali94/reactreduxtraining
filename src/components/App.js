76017379ce50174546d620c507fa06b99787cfdd
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-03 10:29:58 +1000

    finished sec16 lec195 initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import PostList from './PostList';
 
 const App = () => {
-    return (
-        <div className="ui container"><PostList /></div>
-    );
+    return <div>App</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-/* redux-thunk is middleware to help us make requests in a redux application.
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
 import App from './components/App';
-import reducers from './reducers';
-
-/* Below is how we hookup up middleware to our Redux store.
- */
-const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+    <App />, document.querySelector('#root')
 );
