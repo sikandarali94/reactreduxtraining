baffe169647ef0da13c20094ce55260fc5fed0dc
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-09 13:39:07 +1000

    finished sec16 lec206 links inside routers

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,20 @@ import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
 
+/* Because we have a Link element inside the Header component, we have to place it inside the BrowserRouter element. If
+we place it outside, we get the error: "You should not use <Link> outside a <Router>." */
 const App = () => {
     return (
         <div className="ui container">
-            <Header/>
             <h1>Header!</h1>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Header/>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
                 </div>
             </BrowserRouter>
         </div>
