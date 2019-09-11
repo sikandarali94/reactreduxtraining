30e58ed00af81b3c2dda6f595603392ae01fbb7b
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-11 11:30:52 +1000

    finished sec17 lec211 sending a user into the oauth flow

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 class GoogleAuth extends React.Component {
     componentDidMount() {
-        /* This will just initialize the OAuth library; it's not going to actually take the user through the process.
-         */
+        /* This will just initialize the OAuth library; it's not going to actually take the user through the process. */
+        /* For full documentation of the GAPI library, we can visit:
+        developers.google.com/api-client-library/javascript/reference/referencedocs */
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '299582126959-ocvh4s9g63bgtaegjlvktgcd46ge9u2e.apps.googleusercontent.com',
