072cb2a1b2f6ce9438bec8ba070460d57ce19469
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-13 10:17:28 +1000

    finished sec18 lec224 debug sessions with redux dev tools

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
+/* If we write in the URL: localhost:3000?debug_session=<some_string>, the Redux Dev tools saves all data in Redux Store
+between refreshes of the page. The <some_string> part is used to create separate debug sessions where we can have
+different states to test. */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
