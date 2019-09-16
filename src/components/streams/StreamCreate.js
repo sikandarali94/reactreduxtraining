fc7117a8f3e761ce50744a2461c0d22368a80f58
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-16 15:04:11 +1000

    finished sec19 lec230 automatically handling events

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,13 +4,21 @@ the connect function that is used with the react-redux library. */
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+    renderInput({ input }) {
+        /* The first argument is where we get important info and helpers (e.g. onChange) for our form input. The line
+        below was shortened from: <input onChange={formProps.input.onChange} value={formProps.input.value}/>. The line
+        below is essentially populating the input element with all the properties of input that we destructured. */
+        return <input {...input} />;
+    }
+
+    /* We can make Field any kind of input like text, checkbox, number and so forth depending on the props we pass into
+    the component. We must pass a component (indicating whether the input field is text, checkbox, number, radio button,
+    etc.) prop into Field, otherwise, we will get an error message. */
     render() {
         return (
             <form>
-                /* We can make Field any kind of input like text, checkbox, number and so forth depending on the props
-                we pass into the component. */
-                <Field name="title" />
-                <Field name="description" />
+                <Field name="title" component={this.renderInput}/>
+                <Field name="description" component={this.renderInput} />
             </form>
         );
     }
@@ -19,6 +27,7 @@ class StreamCreate extends React.Component {
 /* Unlike the connect function which takes separate arguments, reduxForm instead receives a single object where we put
 a bunch of configuration into that object. reduxForm hooks up a ton of props into our component like blur, change,
 dispatch, invalid, and a whole lot more! */
+/* We must provide a name to the form by passing a value to the form key, as shown below. */
 export default reduxForm({
-    form: ''
+    form: 'streamCreate'
 })(StreamCreate);
