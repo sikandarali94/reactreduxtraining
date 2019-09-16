91ab5d11309865d78ddd7d2675ff22ff2a2e4ca7
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-16 13:53:13 +1000

    finished sec19 lec228 connecting redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from "./authReducer";
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    /* We are required to wire up the Redux form reducer to the 'form' key, as shown below. */
+    form: formReducer
 });
