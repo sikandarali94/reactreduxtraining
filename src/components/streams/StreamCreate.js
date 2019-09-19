708b7a759bc3add76f9da5441ad8fbfe0a83211b
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-19 15:33:57 +1000

    finished sec19 lec231 customizing form fields

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -4,21 +4,29 @@ the connect function that is used with the react-redux library. */
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput({ input }) {
+    renderInput({ input, label }) {
         /* The first argument is where we get important info and helpers (e.g. onChange) for our form input. The line
         below was shortened from: <input onChange={formProps.input.onChange} value={formProps.input.value}/>. The line
         below is essentially populating the input element with all the properties of input that we destructured. */
-        return <input {...input} />;
+        return (
+            <div className="field">
+                <label>{ label }</label>
+                <input {...input} />
+            </div>
+        );
     }
 
     /* We can make Field any kind of input like text, checkbox, number and so forth depending on the props we pass into
     the component. We must pass a component (indicating whether the input field is text, checkbox, number, radio button,
     etc.) prop into Field, otherwise, we will get an error message. */
+    /* The label props, because the Field component does not know what to do with it, will get passed onto the
+    component returned by the renderInput method.
+     */
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput}/>
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
             </form>
         );
     }
