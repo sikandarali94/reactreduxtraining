ca8fd422205aa66ab802ba4246c5fae232ca437f
Sikandar Ali <sikandarali94@hotmail.com>
2019-09-23 17:34:18 +1000

    finished sec20 lec239 setting up an api server

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,24 @@
+/* We first should separate our json-server code from our client code. Then in the directory where we want to write the
+server code, we should initialize npm [npm init] and then install json-server using npm (npm install --save
+json-server).
+
+We can set up script commands in the package.json file in the scripts property e.g.
+{
+  "name": "api",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "json-server -p 3001 -w db.json" // This script will run when we write the command: npm start, because we
+    have defined the script under the "start" key.
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "json-server": "^0.15.1"
+  }
+}
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
