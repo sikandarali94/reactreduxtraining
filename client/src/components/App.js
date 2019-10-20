21dbd9ba2f46289b54bd08d395b56afaf2376715
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-20 12:16:38 +1100

    finished sec20 lec257 creating a browser history object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -7,11 +7,18 @@ import StreamList from './streams/StreamList';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
+    /* If we pass history as a prop, the BrowserRouter will attempt to use the provided history object instead of its
+    own default history object. However, by the end, the BrowserRouter will give us a warning saying that it will
+    ignore the history object we passed to it and use its own default history object. To fix this error we should use
+    a plain Router rather than BrowserRouter and pass the history object as a prop to it. Changing from BrowserRouter
+    to a plain Router does not mess up the routing in anyway; the only difference is that we are in control of the
+    history object as opposed to React. */
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header/>
                     <Route path="/" exact component={StreamList}/>
@@ -20,7 +27,7 @@ const App = () => {
                     <Route path="/streams/delete" exact component={StreamDelete}/>
                     <Route path="/streams/show" exact component={StreamShow}/>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
