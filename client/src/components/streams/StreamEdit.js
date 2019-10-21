67eaf65046c8824a472c12fdf66e09de1af7f83d
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-21 11:16:13 +1100

    finished sec20 lec263 selecting records from state

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const StreamEdit = props => {
     console.log(props);
     return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+/* mapStateToProps can be called with two arguments. The second argument gets passed all the props of the component
+connected to the mapStateToProps method (in our case, the props passed to StreamEdit gets passed down into the ownProps
+argument of the mapStateToProps method).
+ */
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps)(StreamEdit);
