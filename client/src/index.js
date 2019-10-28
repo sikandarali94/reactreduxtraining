ad88a32eece755b2154e748e707c2fa5675c5c3f
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-28 16:02:14 +1100

    finished sec21 lec273 more on using portals

--- client/src/index.js
+++ client/src/index.js
@@ -18,6 +18,8 @@ ReactDOM.render(
         <App />
     </Provider>,
     /* With React, normally all components are child of the <div> element with id of 'root'. However, portals allow us
-    to get around this restriction and the reason why we use it. */
+    to get around this restriction and the reason why we use it. Portals allow us to render some element not as a
+    direct child, instead we can render that element or that component as a child of some other element (most commonly
+    the body element) inside our HTML structure. */
     document.querySelector('#root')
 );
