04e1d35efa004083348819f9b7a2cc6f31bbdac1
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-28 17:39:19 +1100

    finished sec21 lec275 hiding a modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = props => {
     /* We use ReactDOM.createPortal() to create a portal. The first argument is the JSX content we want to render; the
@@ -12,9 +13,16 @@ const Modal = props => {
     trying to introduce React into a server-side rendered application, maybe like a Java application that renders HTML
     on the back-end or maybe Ruby on Rails application or Django or something like that. */
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                Modal is here!!!
+        <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+            <div onClick={e => e.stopPropagation()} className="ui standard modal visible active">
+                <div className="header">Delete Stream</div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')
