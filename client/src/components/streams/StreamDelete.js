ed44c696d74ee31057f55bd467a76288e450cbde
Sikandar Ali <sikandarali94@hotmail.com>
2019-10-29 10:35:00 +1100

    finished sec21 lec280 fetching the deletion stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
     /* It's not valid JS syntax to just have two or more elements separate and not contained in a parent element e.g.
 
         <button className="ui button negative">Delete</button>
@@ -25,24 +28,32 @@ const StreamDelete = () => {
         </>
     However, it is a good idea to use <React.Fragment></React.Fragment> as there are some linters out there that see
     empty tags (<></>) as invalid syntax. */
-    const actions = (
-        <React.Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>
-    );
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-    return (
-        <div>
-            StreamDelete
-            <Modal
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-                onDismiss={() => history.push('/')}
-            />
-        </div>
-    );
-};
+    renderActions() {
+        return (
+            <React.Fragment>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment>
+        );
+    }
 
-export default StreamDelete;
+    render() {
+        return (
+            <div>
+                StreamDelete
+                <Modal
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+            </div>
+        );
+    }
+}
+
+export default connect(null, { fetchStream })(StreamDelete);
